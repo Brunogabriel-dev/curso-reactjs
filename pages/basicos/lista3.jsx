cbe2f5c9b9e4857bb712ebc786e3cbf836c24978
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-02-20 10:55:48 -0300

    Inclusão de texto

--- pages/basicos/lista3.jsx
+++ pages/basicos/lista3.jsx
@@ -1,24 +1,10 @@
 function gerarLista() {
   const lista = []
   for(let i = 1; i <= 10; i++) {
-
+    lista.push(<span>{i},</span>)
   }
-  const lista = [
-    <span>1,</span>,
-    <span>2,</span>,
-    <span>3,</span>,
-    <span>4,</span>,
-    <span>5,</span>,
-    <span>6,</span>,
-    <span>7,</span>,
-    <span>8,</span>,
-    <span>9,</span>,
-    <span>10,</span>,
-  ]
-  return 
+  return lista
 }
-
-
 
 export default function lista() {
   return (
