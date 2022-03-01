ca35d2e2c67ae26e638726b8fad980434c8a396b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-01 18:02:32 -0300

    Inclusão de texto

--- pages/basicos/usandoTitulo.jsx
+++ pages/basicos/usandoTitulo.jsx
@@ -8,8 +8,14 @@ export default function usandoTitulo() {
          secundario="Incluir, alterar e excluir coisas!"
       />
       <Titulo
+      principal="Página de login"
+      secundario="Informe o seu email e senha"
+      pequeno={true}
+      />
+      <Titulo
          principal="Página de login"
          secundario="Informe o seu email e senha"
+         pequeno
       />
     </div>
   )
