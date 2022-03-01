bfb25a6794cc096598d3bd7fa907ba472a530fed
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-01 18:32:07 -0300

    Inclusão de texto

--- pages/css/integracao1.jsx
+++ pages/css/integracao1.jsx
@@ -2,7 +2,8 @@ export default function integracao() {
   return (
     <div>
       <div className="vermelha">Texto #01</div>
-      <div className="azul"></div>
+      <div className="azul">Texto #02</div>
+      <div className="branca">Texto #03</div>
     </div>
   )
 }
