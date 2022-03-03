ffb7383884218bd5041972ad1d97550481ab239d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-02 23:35:11 -0300

    Inclusão de texto

--- pages/css/integracao2.jsx
+++ pages/css/integracao2.jsx
@@ -2,7 +2,7 @@ import styles from "./integracao2.module.css"
 
 export default function integracao2() {
   return (
-    <div>
+    <div id={styles.integracao2}>
       <div className={styles.vermelha}>Texto #01</div>
       <div className={styles.azul}>Texto #02</div>
       <div className={styles.branca}>Texto #03</div>
