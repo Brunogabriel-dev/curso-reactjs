f90063547d9f93355ee45b8b0de2d26ac57afb3e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-02 22:52:53 -0300

    Inclusão de texto

--- pages/css/integracao2.jsx
+++ pages/css/integracao2.jsx
@@ -1,9 +1,11 @@
-export default function integracao() {
+import styles from "./integracao2.module.css"
+
+export default function integracao2() {
   return (
     <div>
-      <div className="vermelha">Texto #01</div>
-      <div className="azul">Texto #02</div>
-      <div className="branca">Texto #03</div>
+      <div className={styles.vermelha}>Texto #01</div>
+      <div className={styles.azul}>Texto #02</div>
+      <div className={styles.branca}>Texto #03</div>
     </div>
   )
 }
