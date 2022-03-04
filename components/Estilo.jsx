66e74e12a23f992392dc43d42a7b98dc881b424d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-03 23:08:11 -0300

    Inclusão de texto

--- components/Estilo.jsx
+++ components/Estilo.jsx
@@ -3,7 +3,8 @@ export default function Estilo(props) {
     <div>
       <h1 style={{
         backgroundColor: props.numero >= 0 ? "#2D2" : "#D22",
-        color: "#fff",
+        color: props.color,
+        textAlign: props.direita ? "right" : "left"
     }}>
         Texto
       </h1>
--- pages/css/usandoEstilo.jsx
+++ pages/css/usandoEstilo.jsx
@@ -3,8 +3,8 @@ import Estilo from "../../components/Estilo";
 export default function usandoEstilo() {
   return (
     <div>
-      <Estilo numero={3} />
-      <Estilo numero={-13} />
+      <Estilo numero={3} color="#000" />
+      <Estilo numero={-13} color="#fff" direita />
     </div>
   )
 }
