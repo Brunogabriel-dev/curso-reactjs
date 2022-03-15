3756e74a4d878dfae75deab73390a6d390586f24
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-14 21:25:03 -0300

    Inclusão de texto

--- components/Estilo.jsx
+++ components/Estilo.jsx
@@ -1,13 +1,19 @@
 export default function Estilo(props) {
+
+  const classeAplicada = props.numero >= 0 ? "azul" : "vermelho"
   return (
     <div>
       <h1 style={{
         backgroundColor: props.numero >= 0 ? "#2d2" : "#d22",
         color: props.color,
         textAlign: props.direita ? "right" : "left"
+
     }}>
-        Texto
+        Texto #01
       </h1>
+      <h2 className={props.numero >= 0 ? "azul" : "vermelho"}>
+        Texto #02
+      </h2>
     </div>
   )
 }
