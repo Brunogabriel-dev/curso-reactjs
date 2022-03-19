1066e15f5e130fb750889997e3795131f03cfb61
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-18 22:22:11 -0300

    Inclusão de texto

--- pages/render/repeticao2.jsx
+++ pages/render/repeticao2.jsx
@@ -2,20 +2,20 @@ import listaProdutos from '../../data/listaProdutos'
 
 export default function repeticao2() {
 
+  const comBorda = {
+    border: "1px solid #000"
+  }
+
   function renderizarLinhas() {
     return listaProdutos.map(produto => {
       return (
         <tr key={produto.id}>
-          <td>{produto.id}</td>
-          <td>{produto.nome}</td>
-          <td>{produto.preco}</td>
+          <td style={comBorda}>{produto.id}</td>
+          <td style={comBorda}>{produto.nome}</td>
+          <td style={comBorda}>{produto.preco}</td>
         </tr>
       )
     })
-  }
- 
-  const comBorda = {
-    border: "1px solid #000"
   }
 
   return (
