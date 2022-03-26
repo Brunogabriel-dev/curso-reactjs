fb3b515f43060226489fbb6780936ddae83233c7
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-25 22:51:50 -0300

    Inclusão de texto

--- pages/render/condicional1.jsx
+++ pages/render/condicional1.jsx
@@ -1,7 +1,16 @@
 export default function condicional1() {
   return (
     <div>
-      condicional1
+      <SomentePar numero={1} />
+      <SomentePar numero={2} />
+      <SomentePar numero={3} />
+      <SomentePar numero={4} />
+      <SomentePar numero={5} />
+      <SomentePar numero={6} />
+      <SomentePar numero={7} />
+      <SomentePar numero={8} />
+      <SomentePar numero={9} />
+      <SomentePar numero={10} />
     </div>
   )
 }
