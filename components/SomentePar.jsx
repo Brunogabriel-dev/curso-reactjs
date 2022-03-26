6d59235a937a59287a53520eaddc438c9228e8ae
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-25 23:12:33 -0300

    Inclusão de texto

--- components/SomentePar.jsx
+++ components/SomentePar.jsx
@@ -1,7 +1,19 @@
 export default function SomentePar(props) {
-  if(props.numero % 2 === 0) {
-    return <h1>{props.numero}</h1>
-  } else {
-    return null
-  }
+  const numeroPar = props.numero % 2 === 0
+  return (
+    <div>
+      {numeroPar ?
+      <span>{props.numero}</span> :
+      null
+      }
+    </div>
+  )
+
+
+
+  // if(props.numero % 2 === 0) {
+    // return <h1>{props.numero}</h1>
+  // } else {
+    // return null
+  //}
 }
