7147ca177978fe0269c0bf198b17332756bb400c
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-28 22:26:41 -0300

    Inclusão de texto

--- pages/basicos/componenteComFilho.jsx
+++ pages/basicos/componenteComFilho.jsx
@@ -1,9 +1,14 @@
+import Item from "../../components/Item";
 import Lista from "../../components/Lista";
 
 export default function componenteComFilho() {
   return (
     <div>
-      <Lista></Lista>
+      <Lista>
+        <Item conteudo="Item #11" />
+        <Item conteudo="Item #12" />
+        <Item conteudo="Item #13" />
+      </Lista>
     </div>
   )
 }
