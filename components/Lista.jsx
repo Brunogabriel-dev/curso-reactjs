7c65e108d3a68ce3392b63bd67b79589a409dc2b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-29 20:56:48 -0300

    Inclusão de texto

--- components/Lista.jsx
+++ components/Lista.jsx
@@ -1,7 +1,13 @@
 export default function Lista(props) {
   return (
-    <ul>
-     {props.children}
-    </ul>
+   <div>
+     <h1>Lista de Algo</h1>
+     <ul style={{
+       listStyle: "none",
+       padding: 0
+     }}>
+       {props.children}
+     </ul>
+   </div>
   )
 }
--- pages/basicos/componenteComFilho.jsx
+++ pages/basicos/componenteComFilho.jsx
@@ -5,6 +5,9 @@ export default function componenteComFilho() {
   return (
     <div>
       <Lista>
+        <Item conteudo="Item #1" />
+        <Item conteudo="Item #2" />
+        <Item conteudo="Item #3" />
         <Item conteudo="Item #11" />
         <Item conteudo="Item #12" />
         <Item conteudo="Item #13" />
