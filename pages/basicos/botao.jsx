da46b39ee189914fff89ebf33a0e03c3a506e025
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-03-30 21:13:52 -0300

    Inclusão de texto

--- pages/basicos/botao.jsx
+++ pages/basicos/botao.jsx
@@ -1,7 +1,19 @@
+function acao1() {
+  console.log("acao1")
+}
+
 export default function botao() {
+  function acao2() {
+    console.log("acao2")
+  }
+  
+  
+  
+  
   return (
     <div>
-      <button>Click</button>
+      <button onClick={acao1}>Click #01</button>
+      <button onClick={acao2}>Click #02</button>
     </div>
   )
 }
