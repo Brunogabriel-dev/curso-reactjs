6dcdb7c115adf1ca47536082452fb8687bbefa15
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-04 21:42:33 -0300

    Inclusão de texto

--- components/indireta1/Filho.jsx
+++ components/indireta1/Filho.jsx
@@ -1,7 +1,10 @@
 export default function Filho(props) {
+  //
+  console.log(props.funcao)
   return (
     <div>
       <h1>Filho</h1>
+      <button onClick={ev => props.funcao("Passei no ENEM!")}>Falar com o Pai</button>
     </div>
   )
 }
--- components/indireta1/Pai.jsx
+++ components/indireta1/Pai.jsx
@@ -1,9 +1,16 @@
 import Filho from "./Filho";
 
 export default function Pai(props) {
+
+  
+function falarComigo(param) {
+  console.log(param)
+}
+
+
   return (
     <div>
-      <Filho />]
+      <Filho funcao={falarComigo}/>
     </div>
   )
 }
