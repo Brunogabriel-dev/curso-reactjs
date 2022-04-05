1539bc937ee204ce86440595cbc29db585c41b26
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-05 19:09:18 -0300

    Inclusão de texto

--- pages/estado/mouse.jsx
+++ pages/estado/mouse.jsx
@@ -1,8 +1,12 @@
 export default function(props) {
   return (
     <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
       backgroundColor: "#222",
-      color: "#fff"
+      color: "#fff",
+      height: "100vh"
     }}>
       mouse
     </div>
