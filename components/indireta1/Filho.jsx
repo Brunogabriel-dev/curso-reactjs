d1f8229663044454c95db53a47e612d746ce9237
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-04 22:31:59 -0300

    Inclusão de texto

--- components/indireta1/Filho.jsx
+++ components/indireta1/Filho.jsx
@@ -4,7 +4,7 @@ export default function Filho(props) {
   return (
     <div>
       <h1>Filho</h1>
-      <button onClick={() => props.funcao}>Falar com o Pai #1</button>
+      <button onClick={props.funcao}>Falar com o Pai #1</button>
       <button onClick={() => props.funcao("Passei no ENEM!")}>Falar com o Pai #2</button>
     </div>
   )
