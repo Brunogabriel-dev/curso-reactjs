73a29807b1f85732a170b3a3603dd33165139046
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-06 22:08:09 -0300

    Inclusão de texto

--- pages/estado/contador.jsx
+++ pages/estado/contador.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+
 export default function contador() {
+  const [numero, setNumero] = useState(0)
+
+  function inc() {
+    setNumero(numero + 1)
+
+  }
+
+  function dec() {
+    setNumero(numero - 1)
+
+  }
+
   return (
     <div>
       <h1>Contador</h1>
+      <div>Valor: {numero}</div>
+      <button>-</button>
+      <button>+</button>
     </div>
   )
 }
