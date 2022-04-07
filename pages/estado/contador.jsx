ea7962bfc36dc1a8de2af0334f41d28239343a83
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-06 22:19:28 -0300

    Inclusão de texto

--- pages/estado/contador.jsx
+++ pages/estado/contador.jsx
@@ -3,22 +3,14 @@ import { useState } from "react"
 export default function contador() {
   const [numero, setNumero] = useState(0)
 
-  function inc() {
-    setNumero(numero + 1)
-
-  }
-
-  function dec() {
-    setNumero(numero - 1)
-
-  }
+  const inc = () => setNumero(numero + 1)
 
   return (
     <div>
       <h1>Contador</h1>
       <div>Valor: {numero}</div>
-      <button>-</button>
-      <button>+</button>
+      <button onClick={dec}>-</button>
+      <button onClick={() => setNumero(numero + 1)}>+</button>
     </div>
   )
 }
