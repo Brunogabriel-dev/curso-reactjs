010fcbcd4b531e523ff1b85afcf61c98fdb38605
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-07 21:07:08 -0300

    Inclusão de texto

--- components/ContadorDisplay.jsx
+++ components/ContadorDisplay.jsx
@@ -2,6 +2,10 @@ export default function ContadorDisplay(props) {
   return (
     <div style={{
       height: "200px",
+      width: "200px",
+      borderRadius: "100px",
+      backgroundColor: "#222",
+      color: "#fff"
     }}>
 
     </div>
