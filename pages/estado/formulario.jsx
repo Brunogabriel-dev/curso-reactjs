5302df6e3b67927630cb0d900e2e51095ee12d29
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-07 22:00:21 -0300

    Inclusão de texto

--- pages/estado/formulario.jsx
+++ pages/estado/formulario.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react"
+
 export default function formulario() {
+const [valor, setValor] = useState("inicial")
+
   return (
     <div>
-      
+     <input type="text" />
     </div>
   )
 }
