b18214e1a6eaa2bdbc8c44b5712018ea6a66a397
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-11 21:29:14 -0300

    Inclusão de texto

--- functions/mega.js
+++ functions/mega.js
@@ -1,6 +1,10 @@
-export default function mega(qtde = 6) {
+export function mega(qtde = 6, numeros = []) {
   if(qtde < 6 && qtde > 60) {
     throw "Quantidade inválida!"
+  }
+
+  if(numeros.length === qtde) {
+    return numeros
   }
 }
 
