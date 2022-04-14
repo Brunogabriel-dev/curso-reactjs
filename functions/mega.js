2f9372eeba7403a1f8dfb3cf0f0b19029320f008
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-13 23:03:15 -0300

    Inclusão de texto

--- functions/mega.js
+++ functions/mega.js
@@ -7,7 +7,7 @@ export function mega(qtde = 6, numeros = []) {
     return numeros
   }
 
-  const numerosAleatorios = paseInt(Math.random() * 60) + 1
+  const numerosAleatorio = paseInt(Math.random() * 60) + 1
   if(!numeros.includes(numerosAleatorio)) {
     return mega(qtde, [...numeros])
   }
