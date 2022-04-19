d075cace086a164540cf4485ee988c15ad00ca7d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-19 16:20:36 -0300

    Inclusão de texto

--- pages/classe/contador.jsx
+++ pages/classe/contador.jsx
@@ -2,4 +2,12 @@ import { Component } from "react";
 
 export default class Contador extends Component{
 
+  render() {
+    return (
+      <div>
+        <h1>Teste classe</h1>
+      </div>
+    )
+  }
+
 }
