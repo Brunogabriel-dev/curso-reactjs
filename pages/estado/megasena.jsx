b63862828f51b129ed9e0fd027dc535dd4badf4d
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-18 21:16:42 -0300

    Inclusão de texto

--- pages/estado/megasena.jsx
+++ pages/estado/megasena.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react/cjs/react.production.min"
+
 export default function megasena() {
+  const [numeros, setNumeros] = useState()
   return (
     <div>
-      
+      <h1>Mega Sena</h1>
     </div>
   )
 }
