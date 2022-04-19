426f78207510b807acc4d0ec3172154ce0e86a45
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-04-18 21:23:56 -0300

    Inclusão de texto

--- pages/estado/megasena.jsx
+++ pages/estado/megasena.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react"
+import NumeroDisplay from "../../components/NumeroDisplay"
 import { mega } from "../../functions/mega"
 
 export default function megasena() {
-  
-  const [numeros, setNumeros] = useState()
+
+  const [numeros, setNumeros] = useState(mega())
+
+
+ function renderNumeros() {
+   return numeros.map(n => <NumeroDisplay />)
+ }
+
   return (
     <div>
       <h1>Mega Sena</h1>
