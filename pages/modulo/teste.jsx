74785a66b5f1a054d46e59420b4ac75fe27da304
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-05-24 22:18:02 -0300

    Inclusão de texto

--- pages/modulo/teste.jsx
+++ pages/modulo/teste.jsx
@@ -1,8 +1,13 @@
-export {} from "../../components/modulo/funcionais"
-export function teste() {
+import 
+Padrao, { Comp1 as Blabla, Comp2 } 
+from "../../components/modulo/funcionais"
+
+export default function teste() {
   return (
     <div>
-
+      <Blabla />
+      <Comp2 />
+      <Padrao />
     </div>
   )
 }
