87b1c467bfa21484711101ab82d3096b5109439e
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-05-24 21:41:11 -0300

    Inclusão de texto

--- components/modulo/funcionais.jsx
+++ components/modulo/funcionais.jsx
@@ -1,7 +1,11 @@
 export function Comp1() {
-  return <h2>Comp1</h2>
+  return (
+    <div>
+      <h2>Comp #01</h2>
+    </div>
+  ) 
 }
 
 export const Comp2 = function() {
-  return <h2>Comp2</h2>
+  return <h2>Comp #02</h2>
 }
