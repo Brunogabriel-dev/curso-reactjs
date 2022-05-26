c0e75daceb2145fa9467f96a63caabd45c1bb189
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-05-25 22:30:23 -0300

    Inclusão de texto

--- components/modulo/funcionais.jsx
+++ components/modulo/funcionais.jsx
@@ -24,10 +24,14 @@ export const Comp4 = () => {
   return <h2>Comp #04</h2>
 }
 
-export const Comp5 = () => <h2>Comp #05</h2>
+const Comp5 = () => <h2>Comp #05</h2>
 
-export const Comp6 = props => (
+const Comp6 = props => (
   <div>
     <h2>Comp #06 - {props.msg}</h2>
   </div>
 )
+
+export {
+  Comp5, Comp6
+}
